2b81099cde885714f00894411f7ce385b84d6b72
Alexey Goroshchenya <375295509897@yandex.ru>
2022-03-23 21:15:15 +0300

    закончил свайпер карусель

--- src/modules/swiper.js
+++ src/modules/swiper.js
@@ -7,20 +7,10 @@ export const swiper = () => {
 
     const swiper = new Swiper('.swiper', {
         slidesPerView: 1,
-        //speed: 2000,
-        //centeredSlides: true,
-        //setWrapperSize: true,
-        width: 185,
-        spaceBetween: 95,
-        translate: 280,
-        virtualTranslate: false,
-        //loop: true,
-        //loopedSlides: 0,
-        loopAdditionalSlides: 10,
         modules: [Autoplay, Navigation],
         autoplay: {
             delay: 2000,
-            disableOnInteraction: true,
+            disableOnInteraction: false,
             stopOnLastSlide: false,
         },
 
@@ -36,5 +26,6 @@ export const swiper = () => {
 
     }
     );
+
 
 }
